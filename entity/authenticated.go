4af3705e32ae67ed8abcf3b925e4eb8186cbff50
victorbetoni <victorbetoni@protonmail.com>
2024-12-27 18:13:49 -0300

    bringing position back

--- entity/authenticated.go
+++ entity/authenticated.go
@@ -6,6 +6,7 @@ type AuthenticatedItem struct {
 	Id           string         `json:"id"`
 	Item         string         `json:"item_name"`
 	Weight       float64        `json:"weight"`
+	Position     int            `json:"position"`
 	OwnerMC      string         `json:"owner_mc_uuid"`
 	OwnerDC      string         `json:"owner_dc_id"`
 	Stats        map[string]int `json:"stats"`
