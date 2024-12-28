9be0a63ee89717d019c7bbd237d3c6272f3294b9
victorbetoni <victorbetoni@protonmail.com>
2024-12-28 13:05:22 -0300

    wrapping wynnitem inside authenticated item

--- entity/authenticated.go
+++ entity/authenticated.go
@@ -10,6 +10,7 @@ type AuthenticatedItem struct {
 	OwnerMC      string         `json:"owner_mc_uuid"`
 	OwnerDC      string         `json:"owner_dc_id"`
 	Stats        map[string]int `json:"stats"`
+	WynnItem     *WynnItem      `json:"wynn_item"`
 	LastRanked   time.Time      `json:"last_weighted"`
 	PublicOwner  bool           `json:"public_owner"`
 	TrackingCode string         `json:"tracking_code"`
